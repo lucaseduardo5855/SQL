c4b835fb3a7e9e790676f6e6c3b947f92aa1bbd7
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-04-05 21:46:19 -0300

    WHERE com LIKE

--- 01/SQL_EXERCICIOS/04_where.sql
+++ 01/SQL_EXERCICIOS/04_where.sql
@@ -1,5 +1,7 @@
 --selecione produtos q tem 'churn' no nome
 
-SELECT * from produtos WHERE DescNomeProduto IN('Botas de Força', 'Botas de Lava', 'Botas de Veludo')
+SELECT * FROM produtos WHERE DescNomeProduto LIKE 'Botas%'
+
+--WHERE DescNomeProduto IN('Botas de Força', 'Botas de Lava', 'Botas de Veludo')
 
 --WHERE DescNomeProduto = 'Botas de Força'OR DescNomeProduto = 'Botas de Lava' OR DescNomeProduto = 'Botas de Veludo'
