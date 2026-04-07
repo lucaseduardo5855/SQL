53a161be98e38e3b23a8e9e174e32958f347c0b1
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-04-06 22:08:16 -0300

    EX 4  Filtro 100 a 200 points

--- 01/Exercicios Where/04_WHERE.sql
+++ 01/Exercicios Where/04_WHERE.sql
@@ -1,2 +1,2 @@
 /* Lista de Clientes com 100 a 200 pontos*/
-SELECT idCliente, qtdePontos FROM clientes WHERE qtdePontos IN ('100', '200')
+SELECT idCliente, qtdePontos FROM clientes WHERE qtdePontos >= 100 AND  qtdePontos <= 200
