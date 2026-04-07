c1130160826f1e1c8fa506790f8f238d2037e0c9
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-04-07 19:37:33 -0300

    DESC e ASC com ORDERY BY

--- 01/06_order_by.sql
+++ 01/06_order_by.sql
@@ -1,2 +1,10 @@
-/* TOP 10 Clientes que mais tem saldo */ 
-SELECT * FROM clientes ORDER BY QtdePontos DESC limit 10
+-- /* TOP 10 Clientes que mais tem saldo */ 
+-- SELECT * FROM clientes ORDER BY QtdePontos DESC LIMIT 10;
+
+
+ SELECT * FROM clientes 
+ 
+ WHERE flTwitch = 1 
+ AND flEmail = 1
+
+ ORDER BY DtCriacao ASC, QtdePontos DESC 
