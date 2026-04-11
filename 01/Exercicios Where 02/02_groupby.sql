fef3b01bb5487da69eef1fc9ae730a67b92f2e27
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-04-11 19:21:33 -0300

    ex - cliente que mais teve ponto mes de maio

---
+++ 01/Exercicios Where 02/02_groupby.sql
@@ -0,0 +1,15 @@
+-- qual cliente juntou mais pontos positivos em 2025-05?
+
+SELECT idCliente,   
+       sum(QtdePontos) AS TotalPontosP
+
+FROM transacoes
+
+WHERE DtCriacao >= '2025-05-01' AND DtCriacao < '2025-06-01'
+AND QtdePontos > 0
+
+GROUP BY IdCliente
+
+ORDER BY sum(QtdePontos) DESC
+
+LIMIT 1
