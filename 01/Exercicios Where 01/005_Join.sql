1d57cce6c7ff7a01bb6cf670797c0aad9b79a695
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-04-17 20:01:39 -0300

    EX05ATT - Cliente mais antigo tem mais frequencia? comparando

--- 01/Exercicios Where 01/005_Join.sql
+++ 01/Exercicios Where 01/005_Join.sql
@@ -1,6 +1,12 @@
 -- Clientes mais antigos tem mais frequencia de transacao?
 
-SELECT idCliente, 
-       CAST(julianday('now') - julianday(substr(DtCriacao, 1, 19)) AS INT) AS DataBase
+SELECT t1.idCliente, 
+       julianday('now') - julianday(substr(t1.DtCriacao, 1, 19)) AS IdadeBase,
+       count(t2.IdTransacao) AS QtdeTransacoes
 
-FROM clientes
+FROM clientes AS t1
+
+LEFT JOIN transacoes AS t2
+ON t1.idCliente = t2.idCliente
+
+GROUP BY t1.idCliente, IdadeBase
