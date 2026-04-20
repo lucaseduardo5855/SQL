54e141dd0afb86227ff74cc07a30116bc12f3040
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-04-20 20:28:10 -0300

    Ex 17 - CTE quem tava no primeiro e no ultimo e a proporcao

--- 17_cte.sql
+++ 17_cte.sql
@@ -14,22 +14,28 @@
 -- ;
 
 WITH tb_cliente_primeiro_dia AS (
-
   SELECT DISTINCT idCliente
   FROM transacoes
   WHERE substr(DtCriacao, 1,10) = '2025-08-25'
-
 ),
 
 tb_cliente_ultimo_dia AS (
-
   SELECT DISTINCT idCliente
   FROM transacoes
   WHERE substr(DtCriacao, 1,10) = '2025-08-29'
+),
 
+tb_join AS (
+  SELECT t1.idCliente AS primCliente,
+         t2.idCliente AS ultCliente
+
+  FROM tb_cliente_primeiro_dia AS t1
+
+  LEFT JOIN tb_cliente_ultimo_dia AS t2
+  ON t1.idCliente = t2.idCliente
 )
 
-SELECT * 
-FROM tb_cliente_primeiro_dia AS t1
-LEFT JOIN tb_cliente_ultimo_dia AS t2
-ON t1.idCliente = t2.idCliente
+SELECT count(primCliente),
+       count(ultCliente),
+      1. * count(ultCliente) / count(primCliente) AS Proporcao
+from tb_join
