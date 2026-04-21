7ef3d011467ae37508e858409c50f7ddb6b4f718
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-04-21 16:31:05 -0300

    Retrovisor - Usando LAG para visulizar e analizar insight de engajamento

--- 01/019_Window_Function.sql
+++ 01/019_Window_Function.sql
@@ -12,9 +12,19 @@ WHERE DtCriacao >= '2025-08-25'
 AND DtCriacao < '2025-08-30' 
 
 GROUP BY idCliente, DtDia
+),
+
+tb_lag AS (
+
+SELECT *,
+        sum(qtTransacao) OVER (PARTITION BY idCliente ORDER BY DtDia) AS AcumuladoTra,
+        lag(qtTransacao) OVER (PARTITION BY IdCliente ORDER BY DtDia) AS LagTransacao
+ 
+FROM tb_cliente_dia
+
 )
 
 SELECT *,
-        sum(qtTransacao) OVER (PARTITION BY idCliente ORDER BY DtDia) AS AcumuladoTra
- 
-FROM tb_cliente_dia
+        1.* qtTransacao / LagTransacao
+
+FROM tb_lag
