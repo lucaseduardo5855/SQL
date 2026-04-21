7aecc8393b5651280bf752877a92e702a1e4f71c
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-04-21 16:47:46 -0300

    De quanto em quanto tempo o pessoal volta para ver o Curso SQL? -

---
+++ 01/020_Window_Function.sql
@@ -0,0 +1,25 @@
+-- De quanto em quanto tempo o pessoal volta para ver o Curso SQL?
+
+WITH cliente_dia AS ( 
+
+  SELECT 
+        DISTINCT idCliente, 
+        substr(DtCriacao, 1,10) AS dtDia
+
+  FROM transacoes
+  WHERE substr(DtCriacao, 1,4) = '2025'
+  ORDER BY IdCliente, dtDia
+
+),
+
+tb_lag AS (
+
+SELECT *,
+        lag(dtDia) OVER (PARTITION BY idCliente ORDER BY dtDia) AS lagDia
+FROM cliente_dia
+
+)
+
+SELECT *,
+        julianday(lagDia)- julianday(dtDia) AS DtDif
+from tb_lag
