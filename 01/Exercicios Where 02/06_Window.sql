da0a99bfa7f379db1d2ba6ff3f5f2232439816b0
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-04-22 19:54:33 -0300

    Complemente ex 6 - Quando chegamos a 3 mil cliente mediante a base

--- 01/Exercicios Where 02/06_Window.sql
+++ 01/Exercicios Where 02/06_Window.sql
@@ -1,4 +1,5 @@
 -- Quantidade de usuarios cadastrador (absoluto acumulado) ao longo do tempo 
+-- Quando que chegamos em 3mil clientes?
 
 WITH tb_dia_cliente AS (
 
@@ -8,9 +9,17 @@ WITH tb_dia_cliente AS (
   from clientes
 
   GROUP BY DtDia
+),
+
+tb_soma_cliente AS (
+
+  SELECT *,
+          sum(QtdeCliente) OVER (ORDER BY DtDia) AS SomAcumuladaCliente
+
+  from tb_dia_cliente
 )
 
-SELECT *,
-        sum(QtdeCliente) OVER (ORDER BY DtDia) AS SomAcumuladaCliente
-
-from tb_dia_cliente
+SELECT * FROM tb_soma_cliente
+WHERE SomAcumuladaCliente > 3000
+ORDER BY DtDia
+LIMIT 1
