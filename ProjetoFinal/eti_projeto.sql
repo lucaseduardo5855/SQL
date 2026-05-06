433b7965f05343f50e461ebbef6411552bbfac9a
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-05-05 21:02:38 -0300

    Projeto Final - Transacoes Históricas

---
+++ ProjetoFinal/eti_projeto.sql
@@ -0,0 +1,23 @@
+/* Quantidade de transações Históricas (vida, d7, d14, d28, d56) */ 
+
+WITH tb_transacoes AS (
+
+SELECT IdTransacao, 
+       idCliente, 
+       QtdePontos,
+       datetime(substr(DtCriacao,1,19)) AS dtCriacao,
+       julianday('now') - julianday(substr(DtCriacao,1,10)) AS diffDate
+
+  from transacoes
+)
+
+SELECT idCliente,
+       count(DISTINCT IdTransacao) AS QtTransacaoVida,
+       count(CASE WHEN diffDate <= 56 THEN IdTransacao END) AS QtTransacaoVida56D,
+       count(CASE WHEN diffDate <= 28 THEN IdTransacao END) AS QtTransacaoVida28D,
+       count(CASE WHEN diffDate <= 14 THEN IdTransacao END) AS QtTransacaoVida14D,
+       count(CASE WHEN diffDate <= 7 THEN IdTransacao END) AS QtTransacaoVida7D
+
+FROM tb_transacoes
+
+GROUP BY idCliente
